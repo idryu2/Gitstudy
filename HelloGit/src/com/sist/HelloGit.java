adcc22dd25421f5eae99148344168740130b241d
idryu2 <idryu2@naver.com>
2016-01-14 09:22:06 +0900

    no

--- HelloGit/src/com/sist/HelloGit.java
+++ HelloGit/src/com/sist/HelloGit.java
@@ -9,6 +9,7 @@ public class HelloGit {
 		System.out.println("For the LichKing");
 		System.out.println("For the AthorKing");
 		System.out.println("fdfd");
+		System.out.println("LOGIN2333");
 	}
 
 }
