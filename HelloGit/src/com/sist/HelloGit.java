71dbcf06f5a8e448e8034d6b71405ab495fb8e65
idryu2 <idryu2@naver.com>
2016-01-14 09:20:04 +0900

    fdfd

--- HelloGit/src/com/sist/HelloGit.java
+++ HelloGit/src/com/sist/HelloGit.java
@@ -8,7 +8,7 @@ public class HelloGit {
 		System.out.println("What's git?");
 		System.out.println("For the LichKing");
 		System.out.println("For the AthorKing");
-		System.out.println("LOGIN");
+		System.out.println("fdfd");
 	}
 
 }
